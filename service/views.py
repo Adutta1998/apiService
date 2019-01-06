2b01573248e6196f68095b5e2ee6349e0ca0ff0d
Avinaba Dutta <duttaavinaba98@gmail.com>
2019-01-06 23:59:55 +0530

    static and media added with regn form

--- service/views.py
+++ service/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 def home(req):
-    return HttpResponse("Working")
+    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))));
+    return render(req,"home.html")
